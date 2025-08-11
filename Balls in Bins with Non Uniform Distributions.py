9b24f54758fb78a3473ac8ee200c50d5ba98f03b
Clocksify <76559829+Clocksify@users.noreply.github.com>
2025-08-11 17:53:03 +0800

    Add files via upload

--- Balls in Bins with Non Uniform Distributions.py
+++ Balls in Bins with Non Uniform Distributions.py
@@ -15,10 +15,29 @@ def uniform_distribution(N):
 def half_doubled_distribution(N):
     #first N/2 bins 2p, remaining bins p
     cutoff = N // 2
-    weights = [2.0 if i < cutoff else 1 for i in range(N)]
-    #change the sign of i < cutoff to shift middle bin to 2p or p for odd N
+    weights = [2.0 if i <= cutoff else 1 for i in range(N)]
+    #for odd N, change the sign of i < cutoff to <= shift middle bin to 2p, otherwise keep as < for middle bin as p
+    #for even N, change to <= to include N/2 + 1 bin
+    #change weight of bin to get 2p/3p/4p etc
     total = sum(weights)
     return [w/total for w in weights]
+
+def half_doubled_distribution_specific(N, heavy_bins=4):
+    #First `heavy_bins` bins get weight=2, the rest get weight=1.
+    #starts from index 0 so first 4 bins is 0, 1, 2, 3
+    #if heavy_bins is None, then use N // 2 as default (for odd number bins, this will not incdue the middle bin)
+
+    if heavy_bins is None:
+        heavy_bins = N // 2
+    
+    weights = [
+        2.0   if i < heavy_bins else 
+        1.0 
+        for i in range(N)
+    ]
+    total = sum(weights)
+    return [w/total for w in weights]
+
 
 def run_one_trial(distribution, q):
     """
@@ -37,10 +56,12 @@ def run_experiment(distribution, q, attempts):
     return collisions / attempts
 
 if __name__ == "__main__":
-    N = 365                  
-    q = 21                   
+    N = 8   
+    #count of number of bins, not maximum index   
+    #setting N = 8 means bins are 0, 1, 2, 3, 4, 5, 6, 7 
+    q = 3            
     attempts = 1000000     
-    distribution = half_doubled_distribution(N)
+    distribution = half_doubled_distribution_specific(N, heavy_bins=7)
 
     collision_prob = run_experiment(distribution, q, attempts)
     print(f"Estimated collision probability (q={q}, N={N}) over {attempts} trials: {collision_prob:.6f}")
